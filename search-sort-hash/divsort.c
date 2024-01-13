452d3a6381f23f6aadd0634923d3f74dd5b5fc2e
Ronit Mehta <109467924+Ronit26Mehta@users.noreply.github.com>
2024-01-13 10:35:30 +0530

    repaired div

--- search-sort-hash/divsort.c
+++ search-sort-hash/divsort.c
@@ -1,57 +1,67 @@
 #include<stdio.h>
 #define max 10
-void mergesort(int array[],int beg,int end);
-void merge(int array[],int beg,int mid,int end);
-void quicksort(int array[],int beg,int end);
-int partition(int array[],int beg,int end);
-void main(){
-    int choice,len,array[max];
+
+void mergesort(int array[], int beg, int end);
+void merge(int array[], int beg, int mid, int end);
+void quicksort(int array[], int beg, int end);
+int partition(int array[], int beg, int end);
+
+int main() {
+    int choice, len, array[max];
     printf("\nEnter size:");
-    scanf("%d",&len);
+    scanf("%d", &len);
+
     printf("\nEnter values:");
-    for (int i = 0; i < len; i++)
-    {
-        scanf("%d",&array[i]);
+    for (int i = 0; i < len; i++) {
+        scanf("%d", &array[i]);
     }
-    do
-    {
-        printf("\n1.merge sort");
-        printf("\n2.quick sort");
-        printf("\n3.redefine array");
-        printf("\n4.exit");
-        printf("\nenter choice:");
-        scanf("%d",&choice);
-        switch (choice)
-        {
-        case 1:
-             mergesort(array,0,len-1);
-             for (int i = 0; i < len; i++)
-             {
-                printf("%d\t",array[i]);
-             }
-              break;
-        case 2:
-            quicksort(array,0,len-1);
-            for (int i = 0; i < len; i++)
-            {
-                printf("%d\t",array[i]);
-            }
-            break;
-        case 3:
-           for (int i = 0; i < len; i++)
-           {
-             scanf("%d",&array[i]);
-           }
-           break;
-        case 4:
-            printf("\nexiting..");
-            break;
-        default:
-            printf("\ninvalid choice");
-            break;
+
+    do {
+        printf("\n1. Merge Sort");
+        printf("\n2. Quick Sort");
+        printf("\n3. Redefine array");
+        printf("\n4. Exit");
+        printf("\nEnter choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                mergesort(array, 0, len - 1);
+                printf("\nSorted array using Merge Sort:\n");
+                for (int i = 0; i < len; i++) {
+                    printf("%d\t", array[i]);
+                }
+                break;
+
+            case 2:
+                quicksort(array, 0, len - 1);
+                printf("\nSorted array using Quick Sort:\n");
+                for (int i = 0; i < len; i++) {
+                    printf("%d\t", array[i]);
+                }
+                break;
+
+            case 3:
+                printf("\nEnter new values for array:");
+                for (int i = 0; i < len; i++) {
+                    scanf("%d", &array[i]);
+                }
+                break;
+
+            case 4:
+                printf("\nExiting...\n");
+                break;
+
+            default:
+                printf("\nInvalid choice\n");
+                break;
         }
+
     } while (choice != 4);
+
+    return 0;
 }
+
 void mergesort(int array[], int beg, int end) {
     int mid;
     if (beg < end) {
@@ -62,97 +72,79 @@ void mergesort(int array[], int beg, int end) {
     }
 }
 
-void merge(int array[],int beg,int mid,int end){
-    int i=beg,j=mid+1,k=0,temp[end],indexes=beg;
-    while ((i<=mid) && (j<=end))
-    {
-        if (array[i] < array[j])
-        {
-            temp[indexes] = array[i];
+void merge(int array[], int beg, int mid, int end) {
+    int i = beg, j = mid + 1, k = 0, temp[end - beg + 1];
+
+    while (i <= mid && j <= end) {
+        if (array[i] < array[j]) {
+            temp[k] = array[i];
             i++;
-        }
-        else
-        {
-            temp[indexes] =array[j];
+        } else {
+            temp[k] = array[j];
             j++;
         }
-        indexes++;
-    }
-    if (i>mid)
-    {
-        while (j<=end)
-        {
-            temp[indexes] = array[j];
-            j++;
-            indexes++;
-        }
-    }
-    else
-    {
-        while (i<=mid)
-        {
-            temp[indexes] = array[i];
-            i++;
-            indexes++;
-        }
-        
-    }
-    while (k<indexes)
-    {
-        array[k] = temp[k];
         k++;
     }
+
+    while (i <= mid) {
+        temp[k] = array[i];
+        i++;
+        k++;
+    }
+
+    while (j <= end) {
+        temp[k] = array[j];
+        j++;
+        k++;
+    }
+
+    for (i = 0; i < k; i++) {
+        array[beg + i] = temp[i];
+    }
 }
-void quicksort(int array[],int beg,int end){
+
+void quicksort(int array[], int beg, int end) {
     int pos;
-    if (beg<end)
-    {
-        pos = partition(array,beg,end);
-        quicksort(array,beg,pos-1);
-        quicksort(array,pos+1,end);
+    if (beg < end) {
+        pos = partition(array, beg, end);
+        quicksort(array, beg, pos - 1);
+        quicksort(array, pos + 1, end);
     }
 }
-int partition(int array[],int beg,int end){
-    int left ,right,loc,flag,temp;
-    left = loc = beg;
+
+int partition(int array[], int beg, int end) {
+    int left, right, loc, temp;
+
+    loc = left = beg;
     right = end;
-    flag = 1;
-    if (flag != 0)
-    {
-        while ((array[loc] < array[right] ) && (right != loc))
-        {
+
+    while (1) {
+        while (array[loc] <= array[right] && loc != right) {
             right--;
-            if (loc == right)
-            {
-                flag = 0;
-            }
-            else if (array[right] < array[loc])
-            {
-                temp = array[right];
-                array[right] = array[loc];
-                array[loc] = temp;
-                loc = right;
-            }
         }
-        if (flag != 0)
-        {
-            while ((array[loc]> array[left]) && (left != loc))
-            {
-                left++;
-                if (loc == left)
-                {
-                    flag = 0;
-                }
-                else if (array[left] > array[loc])
-                {
-                    temp = array[left];
-                    array[left] = array[loc];
-                    array[loc]  = temp;
-                    loc = left;
-                }
-            }
+
+        if (loc == right) {
+            break;
+        } else if (array[right] < array[loc]) {
+            temp = array[right];
+            array[right] = array[loc];
+            array[loc] = temp;
+            loc = right;
+        }
+
+        while (array[loc] >= array[left] && loc != left) {
+            left++;
+        }
+
+        if (loc == left) {
+            break;
+        } else if (array[left] > array[loc]) {
+            temp = array[left];
+            array[left] = array[loc];
+            array[loc] = temp;
+            loc = left;
         }
     }
+
     return loc;
-    
 }
